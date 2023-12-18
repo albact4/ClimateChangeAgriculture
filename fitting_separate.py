ba323facd553fd40863a6b7a34711146aa6ce491
albact4 <albaclosat@gmail.com>
2023-12-18 12:20:16 +0100

    Working PCA

--- fitting_separate.py
+++ fitting_separate.py
@@ -7,31 +7,72 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
-# Read data from excel file
+def handle_nan(data, fill_value=None, strategy='mean'):
+    """
+    Handle NaN values in a DataFrame or NumPy array.
+
+    Parameters:
+    - data: DataFrame or NumPy array
+    - fill_value: Value to fill NaN entries with (default: None)
+    - strategy: Strategy to fill NaN values ('mean', 'median', 'mode', or 'custom') (default: 'mean')
+
+    Returns:
+    - DataFrame or NumPy array with NaN values handled
+    """
+
+    if isinstance(data, pd.DataFrame):
+        numeric_columns = data.select_dtypes(include=[np.number]).columns
+        if strategy == 'mean':
+            data[numeric_columns] = data[numeric_columns].fillna(data[numeric_columns].mean())
+        elif strategy == 'median':
+            data[numeric_columns] = data[numeric_columns].fillna(data[numeric_columns].median())
+        elif strategy == 'mode':
+            data[numeric_columns] = data[numeric_columns].fillna(data[numeric_columns].mode().iloc[0])
+        elif strategy == 'custom':
+            data[numeric_columns] = data[numeric_columns].fillna(fill_value)
+        else:
+            raise ValueError("Invalid strategy. Choose 'mean', 'median', 'mode', or 'custom'.")
+    elif isinstance(data, np.ndarray):
+        # Similar modification for NumPy array
+        raise NotImplementedError("Handling NaN for NumPy array needs modification.")
+    else:
+        raise ValueError("Input must be a DataFrame or NumPy array.")
+
+    return data
+
+
+##################################################################################################################################
+
+# READ DATA FROM EXCEL
 # windows computer in office
 # data = pd.read_excel('C:\\Users\\Rojano\Desktop\\ag_census_tracts17.xlsx', sheet_name='ag_census_tracts17')
 # MacOS
 data = pd.read_excel('/Users/alba/Desktop/ag_census_tracts17.xlsx', sheet_name='ag_census_tracts17')
-print("Data Read")
 
 # Specify the columns you want to analyze
-input1 = ['av_norm'] #Correct column name is av_norm_N100 just trying av_norm for now
+input1 = ['av_norm'] 
 input2 = ['b1_pp_tr_m_N100', 'b2_pp_sig_N100', 'b3_pp_ann_N100', 'b5_pp_perr_N100' , 'b6_perun_m_N100']
 output1 = ['b7_pp_st_1_N100', 'b4_pp_perd_N100', 'b12_lndcod_N100']
-input3 = ['ag_t_N100']
+#input3 = ['ag_t_N100']
 current_condition = ['RuCaIndRUR']
 
-zero_handling = ['av_norm', 'b5_pp_perr_N100', 'b4_pp_perd_N100', 'ag_t_N100' ]
+# Special array for columns that need to be zero-handled
+zero_handling = ['b5_pp_perr_N100', 'b4_pp_perd_N100', 'b12_lndcod_N100' ]
 
-for column in zero_handling: 
+# Handle Nan values
+data = handle_nan(data, strategy='mean')
+
+# Log transformation for zero handling
+for column in zero_handling:
     data[column] = np.log1p(data[column])
-print("Handle zeros")
-# After the zero handling loop
-print(data[zero_handling])
+
+
 
 # Join the arrays into one
-columns_to_analyze = input1 + input2 + output1 + input3 + current_condition
-print("Columns joined, start loop")
+columns_to_analyze = input1 + input2 + output1 + current_condition  # + input3
+print(columns_to_analyze)
+
+
 
 # Dictionary to map column abbreviations to full names when displaying maps
 column_names_mapping = {
@@ -44,7 +85,6 @@ column_names_mapping = {
     'b7_pp_st_1_N100': 'Productivity_state_degradation',
     'b4_pp_perd_N100': 'Productivity_performance_degradation',
     'b12_lndcod_N100': 'Land_cover_degradation',
-    'ag_t_N100': 'Total of money expenses',  # Fix this name
     'RuCaIndRUR': 'Rural Capacity Index',
 }
 
@@ -52,12 +92,8 @@ column_names_mapping = {
 output_dir = 'fitting_visualizations'
 os.makedirs(output_dir, exist_ok=True)
 
-
-
-
 # Initialize full_name outside the loop (no need to initialize output_dir again)
 full_name = ""
-
 
 # Loop through each column
 for column_name in columns_to_analyze:
@@ -67,6 +103,7 @@ for column_name in columns_to_analyze:
 
         # Remove non-finite values (NaN, inf, -inf)
         data_column = data_column.replace([np.inf, -np.inf], np.nan).dropna()
+        #print(data[columns_to_analyze])
 
         # Check if there are any missing values after dropping NaN
         if data_column.isnull().values.any():
@@ -117,17 +154,61 @@ for column_name in columns_to_analyze:
 
         # Save the plots to files with the full names
         pdf_plot_filename = os.path.join(output_dir, f'{full_name}_pdf_fit.png')
-        #cdf_plot_filename = os.path.join(output_dir, f'{full_name}_cdf_fit.png')
 
         # Save the PDF and CDF figures
         plt.savefig(pdf_plot_filename)
-        #plt.savefig(cdf_plot_filename)
 
         plt.close()  # Close the figures to avoid overlapping plots
-        print(f"Saved PDF plot as: {pdf_plot_filename}")
-        #print(f"Saved CDF plot as: {cdf_plot_filename}")
+        print(f"Saved plot as: {pdf_plot_filename}")
 
     except RuntimeWarning as e:
         print(f"Warning in column {column_name}: {e}")
 
 
+print(data[columns_to_analyze])
+
+# Create a DataFrame with only the columns to analyze
+data_to_analyze = data[columns_to_analyze]
+
+# Standardize the data for PCA
+scaler = StandardScaler()
+data_standardized = scaler.fit_transform(data_to_analyze)
+
+# Perform PCA
+pca = PCA()
+pca_result = pca.fit_transform(data_standardized)
+
+# Plot the explained variance ratio
+plt.figure(figsize=(10, 6))
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('Number of Principal Components')
+plt.ylabel('Cumulative Explained Variance')
+plt.title('Cumulative Explained Variance by Principal Components')
+plt.grid(True)
+plt.show()
+
+# Choose the number of components based on the explained variance
+n_components = 2  # Adjust this based on the plot
+pca = PCA(n_components=n_components)
+pca_result = pca.fit_transform(data_standardized)
+
+# Add PCA components to the DataFrame
+for i in range(n_components):
+    data[f'PCA_Component_{i + 1}'] = pca_result[:, i]
+
+# Perform KMeans clustering
+n_clusters = 3  # Adjust this based on your analysis
+kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+data['Cluster'] = kmeans.fit_predict(data_standardized)
+
+# Visualize PCA components by cluster
+plt.figure(figsize=(12, 6))
+for cluster in range(n_clusters):
+    cluster_data = data[data['Cluster'] == cluster]
+    plt.scatter(cluster_data['PCA_Component_1'], cluster_data['PCA_Component_2'], label=f'Cluster {cluster + 1}')
+
+plt.title('PCA Components by Cluster')
+plt.xlabel('PCA Component 1')
+plt.ylabel('PCA Component 2')
+plt.legend()
+plt.show()
